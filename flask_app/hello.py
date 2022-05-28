61328e37a5b6a9394dde5d450ec6d7beca0bcf52
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-28 03:43:19 +0100

    fix

--- flask_app/hello.py
+++ flask_app/hello.py
@@ -3,6 +3,7 @@ import os
 sys.path.insert(0, '.')
 
 from flask import Flask, redirect, url_for, request, render_template
+from flask_cors import CORS
 from tachkil import get_tachkil
 from taksim_aroud import get_full_aroud
 # from generation import generate_sentence
@@ -10,13 +11,13 @@ from taksim_aroud import get_full_aroud
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 # @app.route('/success/<name>')
 # def success(name):
 #    return 'welcome %s' % name
 
-@app.route('/')
+@app.route('/test')
 def home():
     return render_template('login.html')
 
@@ -25,15 +26,25 @@ def home():
 def tachkil():
     data = request.get_json()
     line = data['params']['text']
-    res = get_tachkil(line)
-    return res
+    res=[]
+    for l in line:
+        res.append(get_tachkil(l)["predicted"])
+
+    print('\n'.join(res))
+    return '\n'.join(res)
 
 
 @app.route('/ultimateAroud', methods=['POST'])
 def ultimateAroud():
     data = request.get_json()
     line = data['params']['text']
-    res = get_full_aroud(line)
+
+    res = []
+    for l in line:
+        res.append(get_full_aroud(l))
+
+    res = {i:res[i] for i in range(len(res))}
+
     return res
 
 
